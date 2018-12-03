39bc8a574da62a8e1c2ea58776c840374b7edc40
root <root@ff3l.net>
2018-12-03 09:43:05 +0100

    Neu und verbessert: Memcache!

--- getdombycoord.php
+++ getdombycoord.php
@@ -1,17 +1,26 @@
 <?php
-include_once('inc/geophp/geoPHP.inc');
+#exit;
 
-$point = geoPHP::load("POINT(".floatval($_GET['lon'])." ".floatval($_GET['lat']).")",'wkt');
+$memcache= new Memcache;
+$memcache->addServer("127.0.0.1", 11211);
+if ( !$dom=$memcache->get($_GET['lon']."-".$_GET['lat'])) {
+  include_once('inc/geophp/geoPHP.inc');
+
+  $point = geoPHP::load("POINT(".floatval($_GET['lon'])." ".floatval($_GET['lat']).")",'wkt');
 
 
-foreach(scandir('shapes') as $file) {
-  if (!is_dir($file)) {
-    $shape = geoPHP::load(file_get_contents('shapes/'.$file),'json');
-    if ($shape->pointInPolygon($point)) {
-      echo strtok($file,".");
-      exit;
+  foreach(scandir('shapes') as $file) {
+    if (!is_dir($file)) {
+      $shape = geoPHP::load(file_get_contents('shapes/'.$file),'json');
+      if ($shape->pointInPolygon($point)) {
+        $dom = strtok($file,".");
+        break;
+      }
     }
   }
+  $dom = $dom ?: "default";
+  $memcache->set($_GET['lon']."-".$_GET['lat'],$dom);
 }
-echo "default";
+echo $dom;
+
 ?>
